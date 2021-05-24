2f6d162d904900086372f2f8b3bb7f6604a89a10
Ben McFadden <mcfaddenben64@gmail.com>
2021-05-24 01:32:48 -0400

    adds deliberate practice of authorization app

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/users" => "users#show"
+  
+  post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
+
+end
